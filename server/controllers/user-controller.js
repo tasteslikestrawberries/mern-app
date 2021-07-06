eced16aac7f7595367dcb8627cd7cc275fa18428
Anja <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-07-06 18:56:18 +0200

    up

--- server/controllers/user-controller.js
+++
@@ -1,6 +0,0 @@
-//controllers establish a connection between the router and models
-//controlling what happens when we try to Create, Read, Update, or Delete a model instance
-
-//importing mongoose model
-const User = require('../models/user-model')
-
